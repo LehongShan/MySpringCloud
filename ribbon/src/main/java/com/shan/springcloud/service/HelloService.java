e3a30f5264f15e4e858de3763a6205fa580f12d8
LehongShan <419732717@qq.com>
2018-01-23 19:01:50 +0800

    spring cloud ribbon 熔断器监控

--- ribbon/src/main/java/com/shan/springcloud/service/HelloService.java
+++ ribbon/src/main/java/com/shan/springcloud/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.shan.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,7 +13,12 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String demoService(String name) {
         return restTemplate.getForObject("http://service-provider-demo01/hi?name="+name,String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 }
