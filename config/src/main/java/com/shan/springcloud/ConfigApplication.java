37d980d0038dfd1ac92b7aeb96c5ccf9523b9645
LehongShan <419732717@qq.com>
2018-01-25 13:17:06 +0800

    config-server

---
+++ config/src/main/java/com/shan/springcloud/ConfigApplication.java
@@ -0,0 +1,27 @@
+package com.shan.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigApplication {
+
+    /**
+
+
+     /{application}/{profile}[/{label}]
+     /{application}-{profile}.yml
+     /{label}/{application}-{profile}.yml
+     /{application}-{profile}.properties
+     /{label}/{application}-{profile}.properties
+
+     * @param args
+     */
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+}
